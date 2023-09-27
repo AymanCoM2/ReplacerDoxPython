447aed2ad0bfecdcbb14bde561bfa0cc057db184
= <aymancoom3@gmail.com>
2023-09-27 14:31:26 +0300

    b

--- alter1.py
+++ alter1.py
@@ -1,5 +1,6 @@
 from docx import Document
 from docx2pdf import convert
+
 document = Document('Invoice.docx')
 
 dic = {
@@ -9,21 +10,31 @@ dic = {
     '${a_4}': '23.00',
     '${a_5}': 'حبة E',
     '${a_6}': '60',
-    '${a_7}': 'This is Going to be the long text to',
-    # TODO , Replacing this For the Tempalte 
+    '${a_7}': 'أيمن صلاح عبد الرازق يابياسيبلسقلصقصثقلث محمد صلاح علي مصطفى',
+    # TODO , Replacing this For the Template
     '${a_8}': 'abcvvv',
     '${a_9}': '1',
 }
+
 for p in document.paragraphs:
     inline = p.runs
     for i in range(len(inline)):
         text = inline[i].text
         for key in dic.keys():
             if key in text:
-                text = text.replace(key, dic[key])
+                if key == '${a_7}':
+                    # Split the text and insert line breaks
+                    text = text.replace(key, dic[key])
+                    lines = text.split('\n')
+                    for j, line in enumerate(lines):
+                        if j > 0:
+                            p.add_run('\n')  # Add a line break
+                            inline = p.runs  # Update the inline runs
+                        inline[i + j].text = line
+                else:
+                    text = text.replace(key, dic[key])
                 inline[i].text = text
 
 document.save('new.docx')
 
-# https://stackoverflow.com/questions/34779724/python-docx-replace-string-in-paragraph-while-keeping-style/57365257#57365257
-# convert("new.docx", "output.pdf")
+convert("new.docx", "output.pdf")
